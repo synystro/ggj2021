af87a3d51f65c33c0c48c492d47c9938a15f6472
ClawNarok <arthurvgf@gmail.com>
2021-01-29 23:38:34 -0300

    objetos e cena morta

---
+++ Assets/Scripts/DetectorDeObjetos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorDeObjetos : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        if (other.CompareTag("Objeto"))
+            other.GetComponent<Objeto>().Proximidade(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print(other.name);
+        if (other.CompareTag("Objeto"))
+            other.GetComponent<Objeto>().Proximidade(false);
+    }
+}
